7b0bbb0e2e0258efe0bdd4e81ebba8197844c4dd
Thestudio821 <e195720@ie.u-ryukyu.ac.jp>
2021-12-07 18:09:55 +0900

    WIP:bug fix

--- app/src/app.js
+++ app/src/app.js
@@ -6,4 +6,4 @@ import nazo03 from './NazoPlugins/nazo03';
 const arn = new ARNEngine('scene-main');
 arn.registerNazoPlugin('nazo01', nazo01);
 arn.registerNazoPlugin('nazo02', nazo02);
-arn.registerNazoPlugin('nazo01', nazo03);
+arn.registerNazoPlugin('nazo03', nazo03);
