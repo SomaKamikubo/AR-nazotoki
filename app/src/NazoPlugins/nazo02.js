c4bfdb730d64409f79bdafed3a6ee2d0abf5dbd4
Thestudio821 <e195720@ie.u-ryukyu.ac.jp>
2021-12-07 16:53:07 +0900

    Nazo02の場所整理

--- app/src/NazoPlugins/nazo02.js
+++ app/src/NazoPlugins/nazo02.js
@@ -3,11 +3,11 @@ class Nazo02 {
   constructor(){}
   
   init(engine){
-    engine.registerAsset('nazo01-chest-model', '3d-model', './assets/nazo01/chestv3.gltf');
-    engine.registerAsset('nazo01-2-img', 'image', './assets/nazo01/red_kiiro.png');
-    engine.registerMarker('nazo01-map-marker', './assets/nazo01/pattern-kaiga.patt');
-    const chestEntity = engine.createEntity('nazo01-chest-entity', 'nazo01-chest-model', 'nazo01-map-marker', [-2,0,2], [0,180,0]);
-    const hintImg = engine.createEntity('nazo01-2-img-entity', 'nazo01-2-img', 'nazo01-chest-entity', [0, 3, 0]);
+    engine.registerAsset('nazo02-chest-model', '3d-model', './assets/nazo02/chestv3.gltf');
+    engine.registerAsset('nazo02-img', 'image', './assets/nazo02/red_kiiro.png');
+    engine.registerMarker('nazo02-map-marker', './assets/nazo02/pattern-kaiga.patt');
+    const chestEntity = engine.createEntity('nazo02-chest-entity', 'nazo02-chest-model', 'nazo02-map-marker', [-2,0,2], [0,180,0]);
+    const hintImg = engine.createEntity('nazo02-img-entity', 'nazo02-img', 'nazo02-chest-entity', [0, 3, 0]);
     let chestOpend = false;
     chestEntity.addTouchedEventListener(() => {
       if (!chestOpend){
