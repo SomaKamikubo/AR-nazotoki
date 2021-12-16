caa8708015326ec49d10e74fe99f005509941ab0
Kensho14 <e195723@ie.u-ryukyu.ac.jp>
2021-12-16 18:51:11 +0900

    とりあえず接続できるようになった

--- server/index.js
+++ server/index.js
@@ -4,16 +4,20 @@ const { Server } = require('socket.io');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {});
 
 io.on('connection', (socket) => {
   console.log('new connection!');
-  console.log(socket);
+  console.log("initial transport", socket.conn.transport.name); // prints polling or websocket
+  socket.on('echo', (text) => {
+    console.log(`${socket.id}: ${text}`);
+    socket.emit('echo', 'Hello from server!');
+  });
+  socket.on('disconnect', (reason) => {
+    console.log(`disconnect client (${reason})`);
+  });
 });
 
-app.route('/game/*').get((req, res) => {
-  // proxy
-});
-
+app.use('/', express.static('public'));
 
 httpServer.listen(8000);
