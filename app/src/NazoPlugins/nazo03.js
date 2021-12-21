8d6ccc17243ed1f693ec836b6cc5bd1dd9a2a047
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2021-12-21 13:27:08 +0900

    WIP:表示される画像が真ん中になるようにしている途中

--- app/src/NazoPlugins/nazo03.js
+++ app/src/NazoPlugins/nazo03.js
@@ -5,12 +5,7 @@ class Nazo03{
 
 
   init(engine){
-    function showSumEntity(foundNumbers,i){
-      for(const j of [1,2,3,4,5,6]){
-        engine.getEntity(`nazo03-${j}-entity`).visible = false;
-      }     
-      foundNumbers.push(i);
-  
+    function midleCal(foundNumbers){
       let sumPos = [0,0,0];
       for (const number of foundNumbers) {
         for(let s = 0; s < 3; s++){
@@ -21,30 +16,48 @@ class Nazo03{
         return pos/foundNumbers.length;
       })
       
-      const sum = foundNumbers.reduce((prev, curr) => prev + curr, 0);
-      const sumEntity = engine.getEntity(`nazo03-${sum}-entity`);
-      sumEntity.visible = true;
-      sumEntity.position = midlePos;
-      alert(`現在の合計: ${sum}`);
+      return midlePos;
     }
 
     //registAsset
     for (const i of [1,2,3,4,5,6]){
       engine.registerAsset(`nazo03-${i}-png`,'image',`./assets/nazo03/pop${i}.png`);
     }
+
     let foundNumbers = [];
+    
+
     for (const i of [1, 2, 3]){
       const marker = engine.registerMarker(`nazo03-${i}-marker`, `./assets/nazo03/pattern-Number${i}.patt`);
-      const popEntity = engine.createEntity(`nazo03-${i}-entity`, `nazo03-${i}-png`, null, [0,0,-0.5], [0,0,0], [2,2,2]);
+      const popEntity = engine.createEntity(`nazo03-${i}-entity`, `nazo03-${i}-png`, `nazo03-${i}-marker`, [0,0,-0.5], [0,0,0], [2,2,2]);
+      const popEntity2 = engine.createEntity(`nazo03-${i+3}-entity`, `nazo03-${i+3}-png`,null, [0,0,-0.5], [0,0,0], [2,2,2]);
       popEntity.visible = false;
-      const popEntity2 = engine.createEntity(`nazo03-${i+3}-entity`, `nazo03-${i+3}-png`, null, [0,0,-0.5], [0,0,0], [2,2,2]);
       popEntity2.visible = false;
-
+      
+    
       marker.addMarkerFoundEventListener(() => {
-        showSumEntity(foundNumbers,i);
+        for(const j of [1,2,3,4,5,6]){
+          engine.getEntity(`nazo03-${j}-entity`).visible = false;
+        }
+        foundNumbers.push(i);
+        const sum = foundNumbers.reduce((prev, curr) => prev + curr, 0);
+        const sumEntity = engine.getEntity(`nazo03-${sum}-entity`);
+        sumEntity.visible = true;
+        sumEntity.position = midleCal(foundNumbers);
+        alert(`現在の合計: ${sum}`);
       });
+
       marker.addMarkerLostEventListener(() => {
-        showSumEntity(foundNumbers,i);
+        for(const j of [1,2,3,4,5,6]){
+          engine.getEntity(`nazo03-${j}-entity`).visible = false;
+        }
+        foundNumbers = foundNumbers.filter(x => x != i);
+        const sum = foundNumbers.reduce((prev, curr) => prev + curr, 0);
+        const sumEntity = engine.getEntity(`nazo03-${sum}-entity`);
+        sumEntity.visible = true;
+        sumEntity.position = midleCal(foundNumbers);
+        sumEntity.position = marker.position;
+        alert(`現在の合計: ${sum}`);
       });
     }
   }
