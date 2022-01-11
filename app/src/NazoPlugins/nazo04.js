0936e6dc5801b51fa02c598259fbf55243f2061d
Thestudio821 <e195720@ie.u-ryukyu.ac.jp>
2022-01-11 13:51:52 +0900

    0.5秒刻みで,エンティティが回転できるようにした．

--- app/src/NazoPlugins/nazo04.js
+++ app/src/NazoPlugins/nazo04.js
@@ -1,24 +1,31 @@
 class Nazo04 {
   constructor(){}
   init(engine){
+    let lastRot;
+    
     engine.registerAsset('nazo04-player1-in-png', 'image', './assets/nazo04/player1_in.png');
     engine.registerAsset('nazo04-player1-out-png', 'image', './assets/nazo04/player1_out.png');
     const oneMarker = engine.registerMarker('nazo04-player1-marker1', './assets/nazo03/pattern-Number1.patt');
     const twoMarker = engine.registerMarker('nazo04-player1-marker2', './assets/nazo03/pattern-Number2.patt');
-    const outEntity = engine.createEntity('nazo04-player-out', 'nazo04-player1-out-png', 'nazo04-player1-marker1', [0,0.1,-0.5], [-90,0,0], [1,1,1]);
-    const inEntity = engine.createEntity('nazo04-player-in', 'nazo04-player1-in-png', 'nazo04-player1-marker2', oneMarker.getPosition(), [-90,0,0], [1,1,1]);
-
-    //マーカーの角度を検出するためのエンティティ
-    //const testEntity = engine.createEntity('nazo04-player-test', 'nazo04-player1-out-png', 'nazo04-player1-marker2', [0,0.1,-0.5], [-90,0,0], [1,1,1]);
-    
-    oneMarker.addMarkerFoundEventListener(() => {
-      alert(oneMarker.getPosition());
-      inEntity.setPositon(oneMarker.getPosition()); 
+    const outEntity = engine.createEntity('nazo04-player-out', 'nazo04-player1-out-png', 'nazo04-player1-marker1', [0,0,-0.5], [-90,0,0], [1,0.8,1]);
+    ///関数おいて,ポジション用,ローテーション用のマーカーをそれぞれ設定したい.
+    //twoMarkerを傾けると,inEntityのローテーションも変わる.=>回転情報の共有.
+    //twoMarkerの傾きの情報を入手するのではなく,最初に移した際の回転情報を0とする.そこからずらすたびに計算を行う.
+    const inEntity = engine.createEntity('nazo04-player-in', 'nazo04-player1-in-png', 'nazo04-player1-marker1', [0,0.1,-0.5], [-90,0,0], [1,0.8,1]);
+    let changeRotate;
+    twoMarker.addMarkerFoundEventListener(() => {
+      lastRot = 0;
+      console.log("検知したよ");
+      changeRotate = setInterval(() => {
+        inEntity.setRotation(-90,lastRot+0.1,0);
+        lastRot = inEntity.getRotation()[1];
+        console.log(inEntity.getRotation());
+      }, 500);
     });
-    oneMarker.addMarkerLostEventListener(() => {
-      
+    twoMarker.addMarkerLostEventListener(() => {
+      clearInterval(changeRotate);
+      console.log("見失ったよ");
     });
   }
 }
-
 export default new Nazo04();
