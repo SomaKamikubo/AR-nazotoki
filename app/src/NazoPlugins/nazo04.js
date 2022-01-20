0354597ca09eae35a2e794db601b0fe9a1fda012
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2022-01-21 00:31:56 +0900

    entityの角度をmarkerの現在の角度に。

--- app/src/NazoPlugins/nazo04.js
+++ app/src/NazoPlugins/nazo04.js
@@ -5,11 +5,12 @@ class Nazo04 {
     engine.registerAsset('nazo04-player1-out-png', 'image', './assets/nazo04/player1/player1_out.png');
     engine.registerAsset('nazo04-player2-in-png', 'image', './assets/nazo04/player2/player2_in.png');
     engine.registerAsset('nazo04-player2-out-png', 'image', './assets/nazo04/player2/player2_out.png');
-    // //rotationの初期値は,雑な数字に.
 
     function showEntity(foundNumbers,playerNumber){
       engine.getEntity(`nazo04-player1-1`).visible = false;
       engine.getEntity(`nazo04-player1-2`).visible = false;
+      engine.getEntity(`nazo04-player2-1`).visible = false;
+      engine.getEntity(`nazo04-player2-2`).visible = false;
       if(playerNumber === 1){
         const sum = foundNumbers.reduce((prev, curr) => prev + curr, 0);
         if(sum == 3){
@@ -33,12 +34,10 @@ class Nazo04 {
         
           
         }else if(sum == 2){
-          //alert(2)
           engine.getEntity('nazo04-player2-1').visible = false;
           engine.getEntity('nazo04-player2-2').visible = true;
 
         }else if(sum == 1){
-          //alert(1)
           engine.getEntity('nazo04-player2-1').visible = false;
           engine.getEntity('nazo04-player2-2').visible = false;
         }
@@ -49,41 +48,40 @@ class Nazo04 {
     let foundNumbers2 = [];
     let changeRotate;
     let changeRotate2;
-    let entitiyRot;
-    let beforeMarkerRot = null;
+    let count;
+
+  
+
+  
 
     for (const i of [1, 2]){
       const marker = engine.registerMarker(`nazo04-player1-marker${i}`, `./assets/nazo04/player1/player1-kaiga${i}.patt`);
-      const popEntity = engine.createEntity(`nazo04-player1-${i}`, `nazo04-player1-in-png`, `nazo04-player1-marker${i}`, [0,0,-0.5], [-90,0,0], [1.2,1,1.2]);    
-      popEntity.visible = false;
+      engine.createEntity(`nazo04-player1-${i}`, `nazo04-player1-in-png`, `nazo04-player1-marker${i}`, [0,0,-0.5], [-90,0,0], [1.2,1,1.2]);    
       engine.createEntity('nazo04-player-out', 'nazo04-player1-out-png', 'nazo04-player1-marker1', [0,0.1,-0.5], [-90,0,0], [1,0.8,1]);
-
       
-    //markerを見つけた時
+      
+      //markerを見つけた時
       marker.addMarkerFoundEventListener(() => {
         const twoMarker = engine.getMarker('nazo04-player1-marker2');
         const inEntity = engine.getEntity('nazo04-player1-1');
-        const firstRot = twoMarker.getRotation()[2]; //マーカーを見つけた時のマーカの角度
-        console.log("firstRot:"+firstRot*180/Math.PI);
-
+      
         foundNumbers.push(i);
         showEntity(foundNumbers,1);
+
+        //マーカーが二つとも映っていたら
         if(inEntity.visible){
-          console.log("inen true");
-          entitiyRot=0;
+          count=0;
           changeRotate = setInterval(() => {
-            entitiyRot += 1;
-            console.log(entitiyRot);
+            count += 1;
+            console.log(count);
 
             let nowRot = twoMarker.getRotation()[2];
-            let diff = nowRot-firstRot;
-            inEntity.setRotation([-90*Math.PI/180 ,0 ,diff,"XYZ"]);
-            console.log("nowRot:"+nowRot*180/Math.PI+",diff:"+diff*180/Math.PI);
-
-          }, 200);
+            inEntity.setRotation([-90*Math.PI/180 ,0 ,nowRot,"XYZ"]);
+            console.log("nowRot:"+nowRot*180/Math.PI);
+          }, 500);
         }
-        
       });
+
     //markerを見失った時
       marker.addMarkerLostEventListener(() => {
         foundNumbers = foundNumbers.filter(x => x != i);
@@ -95,36 +93,32 @@ class Nazo04 {
     //player2
     for (const i of [1, 2]){
       const marker2 = engine.registerMarker(`nazo04-player2-marker${i}`, `./assets/nazo04/player2/player2-kaiga${i}.patt`);
-      const popEntity = engine.createEntity(`nazo04-player2-${i}`, `nazo04-player2-in-png`, `nazo04-player2-marker${i}`, [0,0,-0.5], [-90,90,0], [1,0.8,1]);    
-      popEntity.visible = false;
+      engine.createEntity(`nazo04-player2-${i}`, `nazo04-player2-in-png`, `nazo04-player2-marker${i}`, [0,0,-0.5], [-90,90,0], [1,0.8,1]);    
       engine.createEntity('nazo04-player-out', 'nazo04-player2-out-png', 'nazo04-player2-marker1', [0,0.1,-0.5], [-90,0,0], [1.2,1,1.2]);
-
-    //markerを見つけた時
+      
+      //markerを見つけた時
       marker2.addMarkerFoundEventListener(() => {
         const twoMarker = engine.getMarker('nazo04-player2-marker2');
         const inEntity = engine.getEntity('nazo04-player2-1');
-        const firstRot = twoMarker.getRotation()[2]; //マーカーを見つけた時のマーカの角度
-        console.log("firstRot:"+firstRot*180/Math.PI);
 
         foundNumbers2.push(i);
         showEntity(foundNumbers2,2);
+        
+        //マーカーが二つとも映っていたら
         if(inEntity.visible){
-          console.log("inen true");
-          entitiyRot=0;
+          count=0;
           changeRotate = setInterval(() => {
-            entitiyRot += 1;
-            console.log(entitiyRot);
+            count += 1;
+            console.log(count);
 
             let nowRot = twoMarker.getRotation()[2];
-            let diff = nowRot-firstRot;
-            inEntity.setRotation([-90*Math.PI/180 ,0 ,diff,"XYZ"]);
-            console.log("nowRot:"+nowRot*180/Math.PI+",diff:"+diff*180/Math.PI);
-
-          }, 200);
+            inEntity.setRotation([-90*Math.PI/180 ,0 ,nowRot,"XYZ"]);
+            console.log("nowRot:"+nowRot*180/Math.PI);
+          }, 500);
         }
-        
       });
-    //markerを見失った時
+
+      //markerを見失った時
       marker2.addMarkerLostEventListener(() => {
         foundNumbers2 = foundNumbers2.filter(x => x != i);
         showEntity(foundNumbers2,2);
@@ -132,43 +126,7 @@ class Nazo04 {
       });
     }
 
-    // engine.registerAsset('nazo04-player1-in-png', 'image', './assets/nazo04/player1_in.png');
-    // engine.registerAsset('nazo04-player1-out-png', 'image', './assets/nazo04/player1_out.png');
-    // const oneMarker = engine.registerMarker('nazo04-player1-marker1', './assets/nazo03/pattern-Number1.patt');
-    // const twoMarker = engine.registerMarker('nazo04-player1-marker2', './assets/nazo03/pattern-Number2.patt');
-    // const outEntity = engine.createEntity('nazo04-player-out', 'nazo04-player1-out-png', 'nazo04-player1-marker1', [0,0,-0.5], [-90,0,0], [1,0.8,1]);
-    // ///関数おいて,ポジション用,ローテーション用のマーカーをそれぞれ設定したい.
-    // //目的:twoMarkerを傾けると,inEntityのローテーションも変わる.=>回転情報の共有.
-    // //twoMarkerの傾きの情報を入手するのではなく,最初に移した際の回転情報を0とする.そこからずらすたびに計算を行う.
-    // const inEntity = engine.createEntity('nazo04-player-in', 'nazo04-player1-in-png', 'nazo04-player1-marker1', [0,0.1,-0.5], [-90,0,0], [1,0.8,1]);
-    // let changeRotate;
-    // let lastRot = 0;
-    // let markerRot = null;
-    // twoMarker.addMarkerFoundEventListener(() => {
-    //   console.log("検知したよ");
-    //   if(markerRot === null){
-    //     markerRot = twoMarker.getRotation();
-    //   }
-    //   changeRotate = setInterval(() => {
-    //     let rot = twoMarker.getRotation()[1];
-    //     if (markerRot){
-    //       let diffRotY = rot - markerRot[1];
-    //       console.log(rot,markerRot[1]);
-    //       let diffRot =Math.abs(diffRotY);
-    //       console.log(diffRotY);
-    //       if (diffRot > 0) {
-    //         inEntity.setRotation(-90,lastRot+diffRotY,0);
-    //         lastRot = inEntity.getRotation()[1];
-    //         console.log(inEntity.getRotation());
-    //       }
-    //     }
-    //     markerRot[1] = rot;
-    //   }, 500);
-    // });
-    // twoMarker.addMarkerLostEventListener(() => {
-    //   clearInterval(changeRotate);
-    //   console.log("見失ったよ");
-    // });
+   
 
     const syncedAreaAnchorMarker = engine.setSyncedAreaAnchor('./assets/share_marker.patt');
     const aMarker = engine.registerSyncedMarker('nazo04-img-a-marker', './assets/nazo04/pattern-Number1.patt');
